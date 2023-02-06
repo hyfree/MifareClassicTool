290e32bfca02bed864b1cd7062699206f6df936a
hyfree <wangxianqiangcn@foxmail.com>
2023-02-06 13:10:21 +0800

    check M0 adn M4 card

--- Mifare Classic Tool/app/src/main/java/de/syss/MifareClassicTool/Activities/TagInfoTool.java
+++ Mifare Classic Tool/app/src/main/java/de/syss/MifareClassicTool/Activities/TagInfoTool.java
@@ -46,6 +46,7 @@ import de.syss.MifareClassicTool.R;
  * Display tag info like technology, size, sector count, etc.
  * This is the only thing a user can do with a device that does not support
  * MIFARE Classic.
+ *
  * @author Gerhard Klostermeier
  */
 public class TagInfoTool extends BasicActivity {
@@ -65,7 +66,7 @@ public class TagInfoTool extends BasicActivity {
 
         mLayout = findViewById(R.id.linearLayoutTagInfoTool);
         mErrorMessage = findViewById(
-                R.id.textTagInfoToolErrorMessage);
+            R.id.textTagInfoToolErrorMessage);
         updateTagInfo(Common.getTag());
     }
 
@@ -83,8 +84,9 @@ public class TagInfoTool extends BasicActivity {
 
     /**
      * Show a dialog with further information.
+     *
      * @param view The View object that triggered the method
-     * (in this case the read more button).
+     *             (in this case the read more button).
      */
     public void onReadMore(View view) {
         int titleID = 0;
@@ -103,24 +105,25 @@ public class TagInfoTool extends BasicActivity {
             return;
         }
         CharSequence styledText = HtmlCompat.fromHtml(
-                getString(messageID), HtmlCompat.FROM_HTML_MODE_LEGACY);
+            getString(messageID), HtmlCompat.FROM_HTML_MODE_LEGACY);
         AlertDialog ad = new AlertDialog.Builder(this)
-        .setTitle(titleID)
-        .setMessage(styledText)
-        .setIcon(android.R.drawable.ic_dialog_alert)
-        .setPositiveButton(R.string.action_ok,
+            .setTitle(titleID)
+            .setMessage(styledText)
+            .setIcon(android.R.drawable.ic_dialog_alert)
+            .setPositiveButton(R.string.action_ok,
                 (dialog, which) -> {
                     // Do nothing.
                 })
-         .show();
+            .show();
         // Make links clickable.
-        ((TextView)ad.findViewById(android.R.id.message)).setMovementMethod(
-                LinkMovementMethod.getInstance());
+        ((TextView) ad.findViewById(android.R.id.message)).setMovementMethod(
+            LinkMovementMethod.getInstance());
     }
 
     /**
      * Update and display the tag information.
      * If there is no MIFARE Classic support, a warning will be shown.
+     *
      * @param tag A Tag from an NFC Intent.
      */
     @SuppressLint("SetTextI18n")
@@ -135,10 +138,10 @@ public class TagInfoTool extends BasicActivity {
             // Create views and add them to the layout.
             TextView headerGenericInfo = new TextView(this);
             headerGenericInfo.setText(Common.colorString(
-                    getString(R.string.text_generic_info),
-                    ContextCompat.getColor(this, R.color.blue)));
+                getString(R.string.text_generic_info),
+                ContextCompat.getColor(this, R.color.blue)));
             TextViewCompat.setTextAppearance(headerGenericInfo,
-                    android.R.style.TextAppearance_Large);
+                android.R.style.TextAppearance_Large);
             headerGenericInfo.setGravity(Gravity.CENTER_HORIZONTAL);
             int pad = Common.dpToPx(5); // 5dp to px.
             headerGenericInfo.setPadding(pad, pad, pad, pad);
@@ -146,7 +149,7 @@ public class TagInfoTool extends BasicActivity {
             TextView genericInfo = new TextView(this);
             genericInfo.setPadding(pad, pad, pad, pad);
             TextViewCompat.setTextAppearance(genericInfo,
-                    android.R.style.TextAppearance_Medium);
+                android.R.style.TextAppearance_Medium);
             mLayout.addView(genericInfo);
             // Get generic info and set these as text.
             String uid = Common.bytes2Hex(tag.getId());
@@ -162,22 +165,22 @@ public class TagInfoTool extends BasicActivity {
             // Swap ATQA to match the common order like shown here:
             // http://nfc-tools.org/index.php?title=ISO14443A
             byte[] atqaBytes = nfca.getAtqa();
-            atqaBytes = new byte[] {atqaBytes[1], atqaBytes[0]};
+            atqaBytes = new byte[]{atqaBytes[1], atqaBytes[0]};
             String atqa = Common.bytes2Hex(atqaBytes);
             // SAK in big endian.
-            byte[] sakBytes = new byte[] {
-                    (byte)((nfca.getSak() >> 8) & 0xFF),
-                    (byte)(nfca.getSak() & 0xFF)};
+            byte[] sakBytes = new byte[]{
+                (byte) ((nfca.getSak() >> 8) & 0xFF),
+                (byte) (nfca.getSak() & 0xFF)};
             String sak;
             // Print the first SAK byte only if it is not 0.
             if (sakBytes[0] != 0) {
                 sak = Common.bytes2Hex(sakBytes);
             } else {
-                sak = Common.bytes2Hex(new byte[] {sakBytes[1]});
+                sak = Common.bytes2Hex(new byte[]{sakBytes[1]});
             }
             String ats = "-";
             IsoDep iso = IsoDep.get(tag);
-            if (iso != null ) {
+            if (iso != null) {
                 byte[] atsBytes = iso.getHistoricalBytes();
                 if (atsBytes != null && atsBytes.length > 0) {
                     ats = Common.bytes2Hex(atsBytes);
@@ -193,74 +196,80 @@ public class TagInfoTool extends BasicActivity {
             }
 
             int hc = ContextCompat.getColor(this, R.color.blue);
-            String cardType="";
-            switch (sak){
+            String cardType = "";
+            switch (sak) {
+                case "00":
+                    cardType += "M0 Card";
+                    break;
                 case "08":
-                    cardType+="M1 Card";
+                    cardType += "M1 Card";
+                    break;
+                case "18":
+                    cardType += "M4 Card";
                     break;
                 case "20":
-                    cardType+="CPU Card";
+                    cardType += "CPU Card";
                     break;
                 case "28":
-                    cardType+="MF1 Card";
+                    cardType += "MF1 Card";
                     break;
                 case "19":
-                    cardType +="ADEL Card";
+                    cardType += "ADEL Card";
                     break;
                 default:
-                    cardType +="Card";
+                    cardType += "Card";
                     break;
 
             }
 
             genericInfo.setText(TextUtils.concat(
-                    Common.colorString(getString(R.string.text_uid) + ":", hc),
-                    "\n", uid, "\n",
-                    Common.colorString(getString(
-                            R.string.text_rf_tech) + ":", hc),
-                    // Tech is always ISO 14443a due to NFC Intent filter.
-                    "\n", getString(R.string.text_rf_tech_14a), "\n",
-                    Common.colorString(getString(R.string.text_atqa) + ":", hc),
-                    "\n", atqa, "\n",
-                    Common.colorString(getString(R.string.text_sak) + ":", hc),
-                    "\n", sak, "\n",
-                    Common.colorString(getString(R.string.text_cardType) + ":", hc),
-                    "\n",cardType, "\n",
-                    Common.colorString(getString(
-                            R.string.text_ats) + ":", hc),
-                    "\n", ats, "\n",
-                    Common.colorString(getString(
-                            R.string.text_tag_type_and_manuf) + ":", hc),
-                    "\n", tagType));
+                Common.colorString(getString(R.string.text_uid) + ":", hc),
+                "\n", uid, "\n",
+                Common.colorString(getString(
+                    R.string.text_rf_tech) + ":", hc),
+                // Tech is always ISO 14443a due to NFC Intent filter.
+                "\n", getString(R.string.text_rf_tech_14a), "\n",
+                Common.colorString(getString(R.string.text_atqa) + ":", hc),
+                "\n", atqa, "\n",
+                Common.colorString(getString(R.string.text_sak) + ":", hc),
+                "\n", sak, "\n",
+                Common.colorString(getString(R.string.text_cardType) + ":", hc),
+                "\n", cardType, "\n",
+                Common.colorString(getString(
+                    R.string.text_ats) + ":", hc),
+                "\n", ats, "\n",
+                Common.colorString(getString(
+                    R.string.text_tag_type_and_manuf) + ":", hc),
+                "\n", tagType));
 
             // Add message that the tag type might be wrong.
             if (tagTypeResourceID != R.string.tag_unknown) {
                 TextView tagTypeInfo = new TextView(this);
                 tagTypeInfo.setPadding(pad, 0, pad, pad);
                 tagTypeInfo.setText(
-                        "(" + getString(R.string.text_tag_type_guess) + ")");
+                    "(" + getString(R.string.text_tag_type_guess) + ")");
                 mLayout.addView(tagTypeInfo);
             }
 
             LinearLayout layout = findViewById(
-                    R.id.linearLayoutTagInfoToolSupport);
+                R.id.linearLayoutTagInfoToolSupport);
             // Check for MIFARE Classic support.
             if (mMFCSupport == 0) {
                 // Display MIFARE Classic info.
                 // Create views and add them to the layout.
                 TextView headerMifareInfo = new TextView(this);
                 headerMifareInfo.setText(Common.colorString(
-                        getString(R.string.text_mf_info),
-                        ContextCompat.getColor(this, R.color.blue)));
+                    getString(R.string.text_mf_info),
+                    ContextCompat.getColor(this, R.color.blue)));
                 TextViewCompat.setTextAppearance(headerMifareInfo,
-                        android.R.style.TextAppearance_Large);
+                    android.R.style.TextAppearance_Large);
                 headerMifareInfo.setGravity(Gravity.CENTER_HORIZONTAL);
                 headerMifareInfo.setPadding(pad, pad * 2, pad, pad);
                 mLayout.addView(headerMifareInfo);
                 TextView mifareInfo = new TextView(this);
                 mifareInfo.setPadding(pad, pad, pad, pad);
                 TextViewCompat.setTextAppearance(mifareInfo,
-                        android.R.style.TextAppearance_Medium);
+                    android.R.style.TextAppearance_Medium);
                 mLayout.addView(mifareInfo);
 
                 // Get MIFARE info and set these as text.
@@ -269,19 +278,19 @@ public class TagInfoTool extends BasicActivity {
                 String sectorCount = "" + mfc.getSectorCount();
                 String blockCount = "" + mfc.getBlockCount();
                 mifareInfo.setText(TextUtils.concat(
-                        Common.colorString(getString(
-                                R.string.text_mem_size) + ":", hc),
-                        "\n", size, " byte\n",
-                        Common.colorString(getString(
-                                R.string.text_block_size) + ":", hc),
-                        // Block size is always 16 byte on MIFARE Classic Tags.
-                        "\n", "" + MifareClassic.BLOCK_SIZE, " byte\n",
-                        Common.colorString(getString(
-                                R.string.text_sector_count) + ":", hc),
-                        "\n", sectorCount, "\n",
-                        Common.colorString(getString(
-                                R.string.text_block_count) + ":", hc),
-                        "\n", blockCount));
+                    Common.colorString(getString(
+                        R.string.text_mem_size) + ":", hc),
+                    "\n", size, " byte\n",
+                    Common.colorString(getString(
+                        R.string.text_block_size) + ":", hc),
+                    // Block size is always 16 byte on MIFARE Classic Tags.
+                    "\n", "" + MifareClassic.BLOCK_SIZE, " byte\n",
+                    Common.colorString(getString(
+                        R.string.text_sector_count) + ":", hc),
+                    "\n", sectorCount, "\n",
+                    Common.colorString(getString(
+                        R.string.text_block_count) + ":", hc),
+                    "\n", blockCount));
                 layout.setVisibility(View.GONE);
             } else if (mMFCSupport == -1) {
                 // No MIFARE Classic Support (due to the device hardware).
@@ -304,7 +313,7 @@ public class TagInfoTool extends BasicActivity {
             mLayout.removeAllViews();
             mLayout.addView(text);
             Toast.makeText(this, R.string.info_no_tag_found,
-                    Toast.LENGTH_SHORT).show();
+                Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -312,9 +321,10 @@ public class TagInfoTool extends BasicActivity {
      * Get (determine) the tag type resource ID from ATQA + SAK + ATS.
      * If no resource is found check for the tag type only on ATQA + SAK
      * (and then on ATQA only).
+     *
      * @param atqa The ATQA from the tag.
-     * @param sak The SAK from the tag.
-     * @param ats The ATS from the tag.
+     * @param sak  The SAK from the tag.
+     * @param ats  The ATS from the tag.
      * @return The resource ID.
      */
     private int getTagIdentifier(String atqa, String sak, String ats) {
@@ -323,18 +333,18 @@ public class TagInfoTool extends BasicActivity {
 
         // First check on ATQA + SAK + ATS.
         int ret = getResources().getIdentifier(
-                prefix + atqa + sak + ats, "string", getPackageName());
+            prefix + atqa + sak + ats, "string", getPackageName());
 
         if (ret == 0) {
             // Check on ATQA + SAK.
             ret = getResources().getIdentifier(
-                    prefix + atqa + sak, "string", getPackageName());
+                prefix + atqa + sak, "string", getPackageName());
         }
 
         if (ret == 0) {
             // Check on ATQA.
             ret = getResources().getIdentifier(
-                    prefix + atqa, "string", getPackageName());
+                prefix + atqa, "string", getPackageName());
         }
 
         if (ret == 0) {
